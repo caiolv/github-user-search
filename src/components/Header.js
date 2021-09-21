4ca53cf5d51d507dfd0b59892379a02e234927ca
Caio Oliveira <caiolv@me.com>
2021-09-21 17:41:43 -0300

    update: header to use bootstrap

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,32 @@ import React from 'react';
 
 import GithubLogo from '../images/github-logo.svg';
 
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
 export default function Header() {
     return (
-        <div id="header">
-            <a href="/">
-                <img src={GithubLogo} alt="GitHub logo" />
-                <h1>GitHub User Search</h1>
-            </a>
-        </div>
+        <Navbar bg="dark" variant="dark" expand="lg">
+            <Container>
+                <Navbar.Brand href="/">
+                    <img
+                        alt=""
+                        src={GithubLogo}
+                        width="50"
+                        height="50"
+                    />{' '}
+                    GitHub User Search
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Teste</Nav.Link>
+
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     )
 }
