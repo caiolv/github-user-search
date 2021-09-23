191712b6355272a2cbff650176747e7058e929e9
Caio Oliveira <caiolv@me.com>
2021-09-23 10:57:03 -0300

    fix: responsiveness

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -18,10 +18,10 @@ export default function Home() {
         <Container fluid id="home" className="d-flex justify-content-center">
             <img id="logo" src={GithubLogo} alt="GitHub Logo" className="img-fluid" />
             <Row className="d-flex justify-content-center">
-                <Col md="6" s="12" xs="10">
+                <Col xs="10" s="12" md="6" lg="6" xl="4" className="mb-0">
                     <Form.Control type="text" placeholder="Buscar por usuário" />
                 </Col>
-                <Col md="2" s="12" className="d-flex justify-content-center mt-x-3">
+                <Col xs="4" s="12" md="2" lg="2" xl="2" className="d-flex justify-content-center mt-md-0 mt-sm-4 mt-xs-4 ">
                     <button type="submit">Buscar <img src={searchIcon} alt="Search Icon" /></button>
                 </Col>
             </Row>
