7fec9ff6480fed877ade236ab2de8ef9a461e31a
Caio Oliveira <caiolv@me.com>
2021-09-23 10:57:23 -0300

    add: api

---
+++ src/services/api.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'https://api.github.com/',
+});
+
+api.interceptors.request.use(config => {
+    config.params = {
+        client_id: process.env.REACT_APP_CLIENT_ID,
+        client_secret: process.env.REACT_APP_CLIENT_SECRET,
+        ...config.params,
+    };
+    config.headers = {
+        "Authorization": `token ${process.env.REACT_APP_API_KEY}`,
+    }
+    return config;
+});
+
+export default api;
