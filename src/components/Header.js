13ab4d4331feb588ae612be965e4ccfeb6fbde2c
Caio Oliveira <caiolv@me.com>
2021-09-23 17:10:09 -0300

    fix: clear unused improt

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,11 +5,10 @@ import GithubLogo from '../images/github-logo.svg';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export default function Header() {
     return (
-        <Navbar bg="dark" variant="dark" expand="lg">
+        <Navbar bg="dark" variant="dark" expand="md">
             <Container>
                 <Navbar.Brand href="/">
                     <img
