74f94ff4cb118377e91d5dcaf75e607e2ef402ef
Caio Oliveira <caiolv@me.com>
2021-09-24 17:37:53 -0300

    fix: check if .env variables

--- src/services/api.js
+++ src/services/api.js
@@ -7,14 +7,17 @@ const api = axios.create({
 });
 
 api.interceptors.request.use(config => {
-    config.params = {
-        client_id: process.env.REACT_APP_CLIENT_ID,
-        client_secret: process.env.REACT_APP_CLIENT_SECRET,
-        ...config.params,
-    };
-    config.headers = {
-        "Authorization": `token ${process.env.REACT_APP_API_KEY}`,
-    }
+    if (process.env.REACT_APP_CLIENT_ID && process.env.REACT_APP_CLIENT_SECRET)
+        config.params = {
+            client_id: process.env.REACT_APP_CLIENT_ID,
+            client_secret: process.env.REACT_APP_CLIENT_SECRET,
+            ...config.params,
+        };
+
+    if (process.env.REACT_APP_API_KEY)
+        config.headers = {
+            "Authorization": `token ${process.env.REACT_APP_API_KEY}`,
+        }
     return config;
 });
 
